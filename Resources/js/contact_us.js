7dff19108fe4e77330e12c697579e0711ca4d2c7
Srikanth Pasam <10387794@mydbs.ie>
2019-11-10 15:08:13 +0000

    form

--- Resources/js/contact_us.js
+++ Resources/js/contact_us.js
@@ -1,22 +1,40 @@
+
 function message(){
-  var email= document.getElementById('email').value;
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var phone = document.getElementById('phone').value;
+  var message = document.getElementById('message').value;
+
+
+  console.log(name);
   console.log(email);
-  if(email==""){
-    document.getElementById('contact-btn').setAttribute("href", "#pop-info_form");
-      alert(`please input the email and then type message `);
+  console.log(phone);
+
+  if(name==""){
+    document.getElementById('contact-btn').setAttribute("href", "#pop-up-form");
+    alert(`Please provide your name.`);
+  }
+  else if (email=="") {
+    document.getElementById('contact-btn').setAttribute("href", "#pop-up-form");
+    alert(`Please provide your email.`);
+  }
+  else if (phone=="") {
+    document.getElementById('contact-btn').setAttribute("href", "#pop-up-form");
+    alert(`Please provide your phone number.`);
   }
   else {
     document.getElementById('contact-btn').setAttribute("href", "#pop-info");
-      alert(`THANK YOU ! ${email} for contacting us we will get you back soon `);
+      alert(`Thank you, ${name} for contacting us. We will get back to you soon.`);
+      document.getElementById('name').value="";
       document.getElementById('email').value="";
+      document.getElementById('phone').value="";
       document.getElementById('message').value="";
   }
-
-
 }
 
-
 function clear_form(){
+  document.getElementById('name').value="";
   document.getElementById('email').value="";
+  document.getElementById('phone').value="";
   document.getElementById('message').value="";
 }
