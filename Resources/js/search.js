0f06959339a8dc2a17dd77cece7755d2a5852c50
Srikanth Pasam <10387794@mydbs.ie>
2019-11-12 15:20:43 +0000

    search

--- Resources/js/search.js
+++ Resources/js/search.js
@@ -1,9 +1,12 @@
+
+
 var search_form=document.getElementById('search-form');
 search_form.onclick= function search_car(){
-  var year=document.getElementById('search-input').value;
+
+var year=document.getElementById('search-input').value;
 
 
-if(year==2019){
+if(year=='2019'){
 document.getElementById('car-1').classList.remove("search_results");
 document.getElementById('car-2').classList.remove("search_results");
 document.getElementById('car-3').classList.remove("search_results");
@@ -15,7 +18,7 @@ document.getElementById('car-8').classList.add("search_results");
 document.getElementById('car-9').classList.add("search_results");
 document.getElementById('car-10').classList.remove("search_results");
 }
-else if (year==2018) {
+else if (year=='2018') {
   document.getElementById('car-1').classList.add("search_results");
   document.getElementById('car-2').classList.add("search_results");
   document.getElementById('car-3').classList.add("search_results");
@@ -27,7 +30,7 @@ else if (year==2018) {
   document.getElementById('car-9').classList.remove("search_results");
   document.getElementById('car-10').classList.add("search_results");
 }
-else if (year==2017) {
+else if (year=='2017') {
   document.getElementById('car-1').classList.add("search_results");
   document.getElementById('car-2').classList.add("search_results");
   document.getElementById('car-3').classList.add("search_results");
@@ -39,7 +42,7 @@ else if (year==2017) {
   document.getElementById('car-9').classList.add("search_results");
   document.getElementById('car-10').classList.add("search_results");
 }
-else if (year==2015) {
+else if (year=='2015') {
   document.getElementById('car-1').classList.add("search_results");
   document.getElementById('car-2').classList.add("search_results");
   document.getElementById('car-3').classList.add("search_results");
@@ -51,7 +54,7 @@ else if (year==2015) {
   document.getElementById('car-9').classList.add("search_results");
   document.getElementById('car-10').classList.add("search_results");
 }
-else if (year==2012) {
+else if (year=='2012') {
   document.getElementById('car-1').classList.add("search_results");
   document.getElementById('car-2').classList.add("search_results");
   document.getElementById('car-3').classList.add("search_results");
@@ -63,9 +66,7 @@ else if (year==2012) {
   document.getElementById('car-9').classList.add("search_results");
   document.getElementById('car-10').classList.add("search_results");
 }
-
-
-if(year=="Toyota"){
+else if(year=== 'toyota'){
 document.getElementById('car-1').classList.remove("search_results");
 document.getElementById('car-2').classList.add("search_results");
 document.getElementById('car-3').classList.add("search_results");
@@ -77,81 +78,79 @@ document.getElementById('car-8').classList.add("search_results");
 document.getElementById('car-9').classList.add("search_results");
 document.getElementById('car-10').classList.remove("search_results");
 }
-else if (year==2018) {
+else if (year=='honda') {
   document.getElementById('car-1').classList.add("search_results");
-  document.getElementById('car-2').classList.add("search_results");
-  document.getElementById('car-3').classList.add("search_results");
+  document.getElementById('car-2').classList.remove("search_results");
+  document.getElementById('car-3').classList.remove("search_results");
   document.getElementById('car-4').classList.add("search_results");
   document.getElementById('car-5').classList.add("search_results");
-  document.getElementById('car-6').classList.add("search_results");
-  document.getElementById('car-7').classList.remove("search_results");
-  document.getElementById('car-8').classList.add("search_results");
-  document.getElementById('car-9').classList.remove("search_results");
-  document.getElementById('car-10').classList.add("search_results");
-}
-else if (year==2017) {
-  document.getElementById('car-1').classList.add("search_results");
-  document.getElementById('car-2').classList.add("search_results");
-  document.getElementById('car-3').classList.add("search_results");
-  document.getElementById('car-4').classList.remove("search_results");
-  document.getElementById('car-5').classList.add("search_results");
-  document.getElementById('car-6').classList.add("search_results");
-  document.getElementById('car-7').classList.add("search_results");
-  document.getElementById('car-8').classList.add("search_results");
-  document.getElementById('car-9').classList.add("search_results");
-  document.getElementById('car-10').classList.add("search_results");
-}
-else if (year==2015) {
-  document.getElementById('car-1').classList.add("search_results");
-  document.getElementById('car-2').classList.add("search_results");
-  document.getElementById('car-3').classList.add("search_results");
-  document.getElementById('car-4').classList.add("search_results");
-  document.getElementById('car-5').classList.add("search_results");
-  document.getElementById('car-6').classList.add("search_results");
+  document.getElementById('car-6').classList.remove("search_results");
   document.getElementById('car-7').classList.add("search_results");
   document.getElementById('car-8').classList.remove("search_results");
   document.getElementById('car-9').classList.add("search_results");
   document.getElementById('car-10').classList.add("search_results");
 }
-else if (year==2012) {
+else if (year=='bmw') {
   document.getElementById('car-1').classList.add("search_results");
   document.getElementById('car-2').classList.add("search_results");
   document.getElementById('car-3').classList.add("search_results");
   document.getElementById('car-4').classList.add("search_results");
-  document.getElementById('car-5').classList.add("search_results");
-  document.getElementById('car-6').classList.remove("search_results");
+  document.getElementById('car-5').classList.remove("search_results");
+  document.getElementById('car-6').classList.add("search_results");
   document.getElementById('car-7').classList.add("search_results");
   document.getElementById('car-8').classList.add("search_results");
   document.getElementById('car-9').classList.add("search_results");
   document.getElementById('car-10').classList.add("search_results");
 }
-
-
-if(year==2019){
-document.getElementById('car-1').classList.remove("search_results");
+else if (year=='suzuki') {
+  document.getElementById('car-1').classList.add("search_results");
+  document.getElementById('car-2').classList.add("search_results");
+  document.getElementById('car-3').classList.add("search_results");
+  document.getElementById('car-4').classList.add("search_results");
+  document.getElementById('car-5').classList.add("search_results");
+  document.getElementById('car-6').classList.add("search_results");
+  document.getElementById('car-7').classList.add("search_results");
+  document.getElementById('car-8').classList.add("search_results");
+  document.getElementById('car-9').classList.remove("search_results");
+  document.getElementById('car-10').classList.add("search_results");
+}
+else if (year=='gli') {
+  document.getElementById('car-1').classList.remove("search_results");
+  document.getElementById('car-2').classList.add("search_results");
+  document.getElementById('car-3').classList.add("search_results");
+  document.getElementById('car-4').classList.add("search_results");
+  document.getElementById('car-5').classList.add("search_results");
+  document.getElementById('car-6').classList.add("search_results");
+  document.getElementById('car-7').classList.add("search_results");
+  document.getElementById('car-8').classList.add("search_results");
+  document.getElementById('car-9').classList.add("search_results");
+  document.getElementById('car-10').classList.add("search_results");
+}
+else if(year=='civic'){
+document.getElementById('car-1').classList.add("search_results");
 document.getElementById('car-2').classList.remove("search_results");
-document.getElementById('car-3').classList.remove("search_results");
+document.getElementById('car-3').classList.add("search_results");
 document.getElementById('car-4').classList.add("search_results");
-document.getElementById('car-5').classList.remove("search_results");
+document.getElementById('car-5').classList.add("search_results");
 document.getElementById('car-6').classList.add("search_results");
 document.getElementById('car-7').classList.add("search_results");
-document.getElementById('car-8').classList.add("search_results");
+document.getElementById('car-8').classList.remove("search_results");
 document.getElementById('car-9').classList.add("search_results");
-document.getElementById('car-10').classList.remove("search_results");
+document.getElementById('car-10').classList.add("search_results");
 }
-else if (year==2018) {
+else if (year=='city') {
   document.getElementById('car-1').classList.add("search_results");
   document.getElementById('car-2').classList.add("search_results");
-  document.getElementById('car-3').classList.add("search_results");
+  document.getElementById('car-3').classList.remove("search_results");
   document.getElementById('car-4').classList.add("search_results");
   document.getElementById('car-5').classList.add("search_results");
   document.getElementById('car-6').classList.add("search_results");
-  document.getElementById('car-7').classList.remove("search_results");
+  document.getElementById('car-7').classList.add("search_results");
   document.getElementById('car-8').classList.add("search_results");
-  document.getElementById('car-9').classList.remove("search_results");
+  document.getElementById('car-9').classList.add("search_results");
   document.getElementById('car-10').classList.add("search_results");
 }
-else if (year==2017) {
+else if (year=='grandy') {
   document.getElementById('car-1').classList.add("search_results");
   document.getElementById('car-2').classList.add("search_results");
   document.getElementById('car-3').classList.add("search_results");
@@ -163,19 +162,19 @@ else if (year==2017) {
   document.getElementById('car-9').classList.add("search_results");
   document.getElementById('car-10').classList.add("search_results");
 }
-else if (year==2015) {
+else if (year=='x7') {
   document.getElementById('car-1').classList.add("search_results");
   document.getElementById('car-2').classList.add("search_results");
   document.getElementById('car-3').classList.add("search_results");
   document.getElementById('car-4').classList.add("search_results");
-  document.getElementById('car-5').classList.add("search_results");
+  document.getElementById('car-5').classList.remove("search_results");
   document.getElementById('car-6').classList.add("search_results");
   document.getElementById('car-7').classList.add("search_results");
-  document.getElementById('car-8').classList.remove("search_results");
+  document.getElementById('car-8').classList.add("search_results");
   document.getElementById('car-9').classList.add("search_results");
   document.getElementById('car-10').classList.add("search_results");
 }
-else if (year==2012) {
+else if (year=='accord') {
   document.getElementById('car-1').classList.add("search_results");
   document.getElementById('car-2').classList.add("search_results");
   document.getElementById('car-3').classList.add("search_results");
@@ -186,6 +185,42 @@ else if (year==2012) {
   document.getElementById('car-8').classList.add("search_results");
   document.getElementById('car-9').classList.add("search_results");
   document.getElementById('car-10').classList.add("search_results");
+}
+else if (year=='fortuner') {
+  document.getElementById('car-1').classList.add("search_results");
+  document.getElementById('car-2').classList.add("search_results");
+  document.getElementById('car-3').classList.add("search_results");
+  document.getElementById('car-4').classList.add("search_results");
+  document.getElementById('car-5').classList.add("search_results");
+  document.getElementById('car-6').classList.add("search_results");
+  document.getElementById('car-7').classList.remove("search_results");
+  document.getElementById('car-8').classList.add("search_results");
+  document.getElementById('car-9').classList.add("search_results");
+  document.getElementById('car-10').classList.add("search_results");
+}
+else if (year=='ciaz') {
+  document.getElementById('car-1').classList.add("search_results");
+  document.getElementById('car-2').classList.add("search_results");
+  document.getElementById('car-3').classList.add("search_results");
+  document.getElementById('car-4').classList.add("search_results");
+  document.getElementById('car-5').classList.add("search_results");
+  document.getElementById('car-6').classList.add("search_results");
+  document.getElementById('car-7').classList.add("search_results");
+  document.getElementById('car-8').classList.add("search_results");
+  document.getElementById('car-9').classList.remove("search_results");
+  document.getElementById('car-10').classList.add("search_results");
+}
+else if (year=='xli') {
+  document.getElementById('car-1').classList.add("search_results");
+  document.getElementById('car-2').classList.add("search_results");
+  document.getElementById('car-3').classList.add("search_results");
+  document.getElementById('car-4').classList.add("search_results");
+  document.getElementById('car-5').classList.add("search_results");
+  document.getElementById('car-6').classList.add("search_results");
+  document.getElementById('car-7').classList.add("search_results");
+  document.getElementById('car-8').classList.add("search_results");
+  document.getElementById('car-9').classList.add("search_results");
+  document.getElementById('car-10').classList.remove("search_results");
 }
 
 
