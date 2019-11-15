dd2c2dce86b7188b5a5aa095ea73323f32e5aae5
Srikanth Pasam <10387794@mydbs.ie>
2019-11-15 16:22:24 +0000

    JSON

--- Resources/js/car_data.js
+++ Resources/js/car_data.js
@@ -33,6 +33,35 @@ function sign_in(){
   alert(`This page will lead to the sign in section in the future!`)
 }
 
+
+function loadJSON(callback) {
+
+var xobj = new XMLHttpRequest();
+xobj.overrideMimeType("application/json");
+xobj.open('GET', 'data/car_data.json', true);
+xobj.onreadystatechange = function () {
+if (xobj.readyState == 4 && xobj.status == "200") {
+
+// .open will NOT return a value but simply returns undefined in async mode so use a callback
+callback(xobj.responseText);
+
+}
+}
+xobj.send(null);
+
+}
+
+// Call to function with anonymous callback
+loadJSON(function(response) {
+// Do Something with the response e.g.
+//jsonresponse = JSON.parse(response);
+
+// Assuming json data is wrapped in square brackets as Drew suggests
+//console.log(jsonresponse[0].name);
+
+});
+
+
 function start(){
 
   document.getElementById('car-pic-1').src="Resources/img/cars/car1/car_1.jpg";
