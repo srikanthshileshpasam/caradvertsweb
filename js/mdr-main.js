b31cfc8a6288131f01ece4b064798e58e1aa11d1
Srikanth Pasam <10387794@mydbs.ie>
2020-01-19 18:48:38 +0000

    city

---
+++ js/mdr-main.js
@@ -0,0 +1,114 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Validate after type ]*/
+    $('.validate-input .input100').each(function(){
+        $(this).on('blur', function(){
+            if(validate(this) == false){
+                showValidate(this);
+            }
+            else {
+                $(this).parent().addClass('true-validate');
+            }
+        })
+    })
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+           $(this).parent().removeClass('true-validate');
+        });
+    });
+
+     function validate (input) {
+
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+
+        $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>')
+        $('.btn-hide-validate').each(function(){
+            $(this).on('click',function(){
+               hideValidate(this);
+            });
+        });
+    }
+
+
+
+
+
+
+})(jQuery);
+
+$.fn.serializeObject = function()
+{
+
+var o = {};
+var a = this.serializeArray();
+
+$.each(a, function() {
+if (o[this.name]) {
+if (!o[this.name].push) {
+o[this.name] = [o[this.name]];
+}
+o[this.name].push(this.value || '');
+} else {
+o[this.name] = this.value || '';
+}
+});
+console.log(o);
+return o;
+};
+var v = $("#data-entry").serializeObject();
+console.log(v);
+
+
+
+function hideValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).removeClass('alert-validate');
+    $(thisAlert).find('.btn-hide-validate').remove();
+}
+var $form = $('form#data-entry'),
+    url = 'https://script.google.com/macros/s/AKfycbwvh79G9R5RTFE542KCqzqlehy_z1WNW4vV7ox-QaNDtxVgIwPG/exec'
+
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  })
+})
