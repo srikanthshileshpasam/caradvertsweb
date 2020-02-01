6cc18c64853152a9f4c95e31595742f2ed126d1a
Srikanth Pasam <10387794@mydbs.ie>
2020-02-01 21:58:58 +0000

    v2

--- js/tvr-main.js
+++ js/tvr-main.js
@@ -19,6 +19,7 @@
     console.log(o);
     return o;
     };
+
     var v = $("#data-entry").serializeObject();
     console.log(v);
 
@@ -29,21 +30,56 @@
         $(thisAlert).removeClass('alert-validate');
         $(thisAlert).find('.btn-hide-validate').remove();
     }
+
     var $form = $('form#data-entry'),
         url = 'https://script.google.com/macros/s/AKfycbwuKy3XaIgnTX4DoidAuP75V3Vs8QpDucbra5gjPCXEMe3HxIsJ/exec'
+        console.log(form);
 
 
-    $('#submit-form').on('click', function(e) {
-      // e.preventDefault();
-      var jqxhr = $.ajax({
-        url: url,
-        method: "GET",
-        dataType: "json",
-        data: $form.serializeObject()
-})
-
-    })
 
 
+        $('#submit-form').on('click', function(e) {
+            alert("in Ajax 1111");
+            console.log("testttttttttttttttttt");
+          // e.preventDefault();
+          var jqxhr = $.ajax({
+            url: url,
+            method: "GET",
+            dataType: "json",
+            data: $("#data-entry").serialize(),
+            success: function(data){
+              alert("in Ajax");
+                    console.log(data);
+
+                },
+
+                error: function(data){
+                    console.log(data);
+                }
+
+          })
+
+        })
 
 })(jQuery);
+
+
+function onclickFunction(){
+console.log("js function");
+
+var vc = document.getElementById('vc_no').value;
+var rec_no = document.getElementById('rec_no').value;
+var pkg = document.getElementById('pkg').value;
+var amt = document.getElementById('amt').value;
+var rem = document.getElementById('rem').value;
+var pwd = document.getElementById('password-3').value;
+
+ // var cust[]= customerSearch();
+
+console.log(pwd);
+
+var $form = $('#data-entry'),
+    url = 'https://script.google.com/macros/s/AKfycbwuKy3XaIgnTX4DoidAuP75V3Vs8QpDucbra5gjPCXEMe3HxIsJ/exec'
+    console.log($form);
+
+}
